02fd05bc5ff541a4ce2a82471e48110c12c09419
Joe <g132565@gmail.com>
2020-04-05 19:01:58 +0800

    add note

--- app/src/main/java/com/example/test1/MainActivity.java
+++ app/src/main/java/com/example/test1/MainActivity.java
@@ -15,12 +15,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void note1(View view) {
-        Intent intent = new Intent(this,Note1.class);
+        Intent intent = new Intent(this, Note1.class);
         startActivity(intent);
     }
 
     public void note2(View view) {
-        Intent intent = new Intent(this,Note2.class);
+        Intent intent = new Intent(this, Note2.class);
         startActivity(intent);
     }
 }
